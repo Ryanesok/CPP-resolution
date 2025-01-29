6f416ec13923f6da7df9cae89d987bf5a3e423fb
ryanesok <zakkidash@gamil.com>
2025-01-29 18:09:28 +0700

    Compile pada cmd dan vscode

--- CPP-MainProgress/0-helloworld.cpp
+++ CPP-MainProgress/0-helloworld.cpp
@@ -1,7 +1,11 @@
+//library dari I/O pada cpp
 #include <iostream>
+
+//fungsi untuk mempersingkat command standar pada I/O
 using namespace std;
 
+//fungsi utama yang akan dieksekusi pertama kali
 int main(){
-  cout << "Hello, World";
-  return 0;
+  cout << "Hello, World"; //cout adalah command output pada console
+  return 0;               //karena fungsi main bertipe int, maka harus mengembalikan nilai int juga
 }
